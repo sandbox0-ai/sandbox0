d318ed24280a9bad503572c69c657646d1996460
Zuch Huang <langyi.huang.1997@gmail.com>
2026-03-23 05:39:57 +0800

    ui: Pixel Collapsible

--- components/ui/src/index.ts
+++ components/ui/src/index.ts
@@ -6,6 +6,7 @@
 export { PixelBox } from "./components/PixelBox";
 export { PixelButton } from "./components/PixelButton";
 export { PixelCard } from "./components/PixelCard";
+export { PixelCollapsible, PixelCollapsiblePanel } from "./components/PixelCollapsible";
 export { PixelInput } from "./components/PixelInput";
 export { PixelSelect } from "./components/PixelSelect";
 export { PixelBadge } from "./components/PixelBadge";
@@ -18,6 +19,10 @@ export { PixelLayout } from "./components/PixelLayout";
 // Types
 export type { PixelScale } from "./types";
 export type { PixelHeadingAs, PixelHeadingTone, PixelHeadingProps } from "./components/PixelHeading";
+export type {
+  PixelCollapsibleProps,
+  PixelCollapsiblePanelProps,
+} from "./components/PixelCollapsible";
 
 // Utilities
 export { cn, getPixelShadowClass, getPixelFontClass } from "./utils";

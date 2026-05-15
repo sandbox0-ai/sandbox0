250dd555e1eb51131adf56cc54a5c9559f257cca
Zuch Huang <langyi.huang.1997@gmail.com>
2026-05-15 15:33:40 -0500

    Delete local function runtime sandboxes through manager

--- cluster-gateway/pkg/http/server.go
+++ cluster-gateway/pkg/http/server.go
@@ -271,7 +271,7 @@ func NewServer(
 			return sandbox, nil
 		},
 		functionapi.NewHTTPStorageProxyVolumeSnapshotter(cfg.StorageProxyURL, internalAuthGen, snapshotHTTPClient, logger),
-		functionapi.NewHTTPRuntimeController(functionapi.StaticClusterGatewayURLResolver(fmt.Sprintf("http://127.0.0.1:%d", cfg.HTTPPort)), internalAuthGen, snapshotHTTPClient),
+		newLocalRuntimeController(managerClient),
 		logger,
 	)
 
